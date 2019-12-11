35144aa58bfb68b86270221758fbe48470e81d27
duthienkt <blueskythien2010@live.com>
2019-12-11 07:58:09 +0700

    + start new version

--- esp_lamp/esp_lamp.ino
+++ esp_lamp/esp_lamp.ino
@@ -1,218 +1,124 @@
 #include <ESP8266WiFi.h>
+
+#include <DNSServer.h>        //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h> //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>      //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
 #include "stringbuilder.h"
-const char* ssid     = "Diem Thuong";
-const char* password = "ad2h3ttt";
-//const char *ssid = "console.log(SSID)";
-//const char *password = "undefined";
 
+#define AIO_SERVER "mqtt.absol.cf"
+#define AIO_SERVERPORT 1883
 
-#define LED 2
+#define AIO_USERNAME "iNet"
+#define AIO_KEY "12345iNet"
+#define AIO_ID "EGGV4_001"
 
-#define L_RED D1  
-#define L_GREEN D2 
-#define L_BLUE D8 
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_ID, AIO_USERNAME, AIO_KEY);
+WiFiManager wifiManager;
+bool wifiAvailable;
 
-#define R_RED D5
-#define R_GREEN D6
-#define R_BLUE D7
-
-#define HUE_PHARE 1000
-
-
-#define IS_NUM(x) (x >= '0' && x <= '9')
-
-WiFiServer server(80);
-
-
-int COLOR[6];
-char *data;
-char *cmd = "?cmd";
-
-int newColorCode[6];
-int32_t now;
-
-int mapAnalogValue(int c) {
-  return 0 + (c * 1023 / 255);
-}
-
-int mapPharseRed(int32_t val) {
-  int p = val / HUE_PHARE;
-  if (p == 0 || p == 5) return 255;
-  if (p == 1) return 225 - (val - HUE_PHARE) * 225 / HUE_PHARE;
-  if (p == 2 || p == 3) return 0;
-  if (p == 4) return (val - HUE_PHARE * 4) * 225 / HUE_PHARE;
-}
-
-
-void updateNewColorCode()
-{
-  for (int i = 0; i < 6; ++i) {
-    COLOR[i] = newColorCode[i];
-  }
-
-  // I use Ortak Anot led, I must to revert PWM value
-  analogWrite(L_RED, 1023 - mapAnalogValue(COLOR[0]));
-  analogWrite(L_GREEN, 1023 -  mapAnalogValue(COLOR[1]));
-  analogWrite(L_BLUE, 1023 - mapAnalogValue(COLOR[2]));
-
-  analogWrite(R_RED, 1023 - mapAnalogValue(COLOR[3]));
-  analogWrite(R_GREEN, 1023 - mapAnalogValue(COLOR[4]));
-  analogWrite(R_BLUE, 1023 - mapAnalogValue(COLOR[5]));
-
-}
-
-//https://en.wikipedia.org/wiki/Hue
-// Six pharse
-void autoColor() {
-  now = millis();
-  for (int i = 0; i < 6; ++i)
-  {
-    now %= HUE_PHARE * 6;
-    newColorCode[i] = mapPharseRed(now);
-    now += HUE_PHARE;
-  }
-
-  updateNewColorCode();
-}
-
-void alert(int duration, int delayTime, int times)
-{
-  for (int i = 0; i < times; ++i)
-  {
-    digitalWrite(LED, LOW);
-    delay(duration);
-    digitalWrite(LED, HIGH);
-    delay(delayTime);
-  }
-}
+Adafruit_MQTT_Subscribe presenceSub = Adafruit_MQTT_Subscribe(&mqtt, "presence");
 
 void setup()
 {
-
-  pinMode(LED, OUTPUT);
-  autoColor();
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    alert(100, 100, 1);
-    autoColor();
-  }
-  delay(500);
-  WiFi.setAutoReconnect(true);
-  alert(100, 100, 5);
-  server.begin();
-  // maximum length of string is 1023
-  data = stbd_init(1024);
-
+    Serial.begin(9600);
+    wifiManager.autoConnect("EGG_LAMP_CONFIG", "12345");
+    wifiAvailable = WiFi.status() == WL_CONNECTED;
+    WiFi.setAutoReconnect(true);
+    delay(1000);
+    mqtt.subscribe(&presenceSub);
 }
 
-
-// request: http://192.168.1.xx?cmd=255,255,255,255,255,255
-
-void handleClient()
+bool wifi_connect()
 {
-  WiFiClient client = server.available(); // Listen for incoming clients
-  int cmdIndex = 0;
-  int newColorCodeIndex = -1;
-  char lastChar = '_';
-  char c = '_';
-  if (client)
-  {
-    int endlCount = 0;
-    stbd_clear();
-    while (client.connected())
+    wifiAvailable = WiFi.status();
+    if (!wifiAvailable)
     {
-      if (client.available())
-      {
-        lastChar = c;
-        c = client.read();
-        if (cmd[cmdIndex])
+        WiFi.reconnect();
+        int retries = 10;
+        while (WiFi.status() != WL_CONNECTED && (retries >= 0))
         {
-          //not end of cmd
-          if (c == cmd[cmdIndex])
-            cmdIndex++;
-          else
-          {
-            //reset automata
-            cmdIndex = 0;
-          }
+            Serial.print("Retry in ");
+            Serial.println(retries);
+            delay(100);
+            retries--;
         }
-        else
-        {
-          if (newColorCodeIndex < 6)
-          {
-            // reading number
-            if (IS_NUM(c))
-            {
-              if (IS_NUM(lastChar))
-              {
-                newColorCode[newColorCodeIndex] = newColorCode[newColorCodeIndex] * 10 + c - 48;
-              }
-              else
-              {
-                //newNum
-                newColorCodeIndex++;
-                if (newColorCodeIndex < 6)
-                  newColorCode[newColorCodeIndex] = c - '0';
-              }
-            }
-          }
-        }
-
-        if (c == '\r')
-          continue;
-        if (c == '\n')
-          ++endlCount;
-        if (endlCount == 2)
-        {
-          // send data
-          stdbd_print("HTTP/1.1 200 OK\r\n");
-          stdbd_print("Server: ESP8266\r\n");
-          stdbd_print("Access-Control-Allow-Origin: *\r\n");// cross-orgin
-          stdbd_print("Content-type:application/json\r\n");
-          stdbd_print("Connection: close\r\n");
-          stdbd_print("\r\n");
-          // Display the HTML web page
-
-          stdbd_print("{ \"id\":\"Meow\"");
-
-          if (newColorCodeIndex >= 5)
-          {
-            //enought
-            stdbd_print(",\"status\":\"OK\"");
-            updateNewColorCode();
-          }
-          else
-          {
-            stdbd_print(",\"error\":\"NOT_ENOUGHT_COLOR_CODE\"");
-          }
-
-          stdbd_print(", \"color\":[");
-          for (int k = 0; k < 6; ++k)
-          {
-            if (k > 0)
-              stdbd_print(",");
-            stdbd_print(COLOR[k]);
-          }
-          stdbd_print("]");
-
-
-
-          stdbd_print("}\r\n");
-          stdbd_print("\r\n");
-          break;
-        }
-      }
+        delay(500);
+        wifiAvailable = WiFi.status();
     }
-    client.print(data);
+    if (!wifiAvailable)
+    {
+        Serial.println("Wifi is not available!");
+    }
+    return wifiAvailable;
+}
 
-    alert(50, 0, 1);
-    client.stop();
-  }
+bool MQTT_connect()
+{
+    if (!wifiAvailable)
+    {
+        return false;
+    }
+    int8_t ret;
+
+    // Stop if already connected.
+    if (mqtt.connected())
+    {
+        return true;
+    }
+
+    Serial.print("Connecting to MQTT... ");
+
+    uint8_t retries = 3;
+    while ((ret = mqtt.connect()) != 0)
+    { // connect will return 0 for connected
+        Serial.println(mqtt.connectErrorString(ret));
+        Serial.println("Retrying MQTT connection in 5 seconds...");
+        mqtt.disconnect();
+        delay(5000); // wait 5 seconds
+        retries--;
+        if (retries == 0)
+        {
+            // basically die and wait for WDT to reset me
+            while (1)
+                ;
+        }
+    }
+    Serial.println("MQTT Connected!");
+    return true;
 }
 
 void loop()
 {
-  handleClient();
+    if (wifi_connect())
+    {
+        if (MQTT_connect())
+        {
+
+            Adafruit_MQTT_Subscribe *subscription;
+            while ((subscription = mqtt.readSubscription(5000)))
+            {
+                // Check if its the onoff button feed
+                if (subscription == &presenceSub)
+                {
+                    uint8 *data = (uint8 *)presenceSub.lastread;
+                    Serial.print(data[0]);
+                    Serial.print(data[1]);
+                    Serial.print(data[2]);
+                    Serial.println(data[3]);
+                }
+            }
+
+            // ping the server to keep the mqtt connection alive
+            if (!mqtt.ping())
+            {
+                mqtt.disconnect();
+            }
+        }
+    }
+
+    // handleClient();
 }
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+var processFolder = process.cwd().replace(/\\/, '/');
+var relative = path.relative(processFolder, __dirname);
+
+
+function resolveEntry(entryInProject){
+    var entryInProcess = path.join(relative, entryInProject);
+    if (!entryInProcess.startsWith('./')) entryInProcess = './' + entryInProcess;
+    return entryInProcess;
+
+}
+
+var packages = {
+    default: {
+        entry: ["./cli/index.js"],
+        filename: "./dist/absol.js"
+    }
+}
+
+const PACKAGE = 'default';
+
+
+module.exports = {
+    mode: process.env.MODE || "development",
+    // mode: 'production',
+    entry: packages[PACKAGE].entry.map(resolveEntry),
+    output: {
+        path: path.join(__dirname, "."),
+        filename: packages[PACKAGE].filename
+    },
+    resolve: {
+        modules: [
+            path.join(__dirname, './node_modules')
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: { presets: [['@babel/preset-env', { modules: false }]] }
+            },
+            {
+                test: /\.(tpl|txt|xml|rels)$/i,
+                use: 'raw-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    optimization: {
+        // We do not want to minimize our code.
+        minimize: false
+    },
+    devServer: {
+        compress: true
+    },
+    performance: {
+        hints: false
+    }
+};
